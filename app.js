37146ac27e88b4f8924248c8d1b326579a58d035
Andres <andresdomlez@gmail.com>
2020-04-25 21:40:57 +0100

    logging refactor

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ new Vue ({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = []
     },
     attack: function() {
       var damage = this.calculateDamage(3, 10);
       this.monsterHealth -= damage;
       this.turns.unshift({
         isPlayer: true,
-        text: 'Player hits Monster for ' + damage;
+        text: 'Player hits Monster for ' + damage
       })
       if (this.checkWin()) {
         return;
@@ -26,7 +27,12 @@ new Vue ({
       this.monsterAttacks();
     },
     specialAttack: function() {
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      var damage = this.calculateDamage(10, 20)
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster hard for ' + damage
+      })
       if (this.checkWin()) {
         return;
       }
@@ -39,6 +45,10 @@ new Vue ({
       } else {
         this.playerHealth = 100;
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10'
+      });
       this.monsterAttacks();
     },
     giveUp: function() {
@@ -49,7 +59,7 @@ new Vue ({
       this.playerHealth -= damage;
       this.turns.unshift({
         isPlayer: false,
-        text: 'Monster hits Player for ' + damage;
+        text: 'Monster hits Player for ' + damage
       })
 
       this.checkWin();
